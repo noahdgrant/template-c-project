84e71a80b2f2d97ff7ac2a5202b71b3cd2f16b94
Noah Grant <noahdgrant@gmail.com>
2024-10-19 11:26:35 -0400

    Add basic arg parsing

--- src/logger.c
+++ src/logger.c
@@ -11,6 +11,9 @@ static LogLevel m_logger_level = INFO;
 static const char* logger_level_to_string(LogLevel level) {
     const char* level_str;
     switch (level) {
+        case TRACE:
+            level_str = "TRACE";
+            break;
         case DEBUG:
             level_str = "DEBUG";
             break;
@@ -23,6 +26,9 @@ static const char* logger_level_to_string(LogLevel level) {
         case ERROR:
             level_str = "ERROR";
             break;
+        case FATAL:
+            level_str = "FATAL";
+            break;
         default:
             logger(FATAL, __func__, "Unknown logger level: %d", level);
             exit(1);
@@ -32,8 +38,10 @@ static const char* logger_level_to_string(LogLevel level) {
 }
 
 LogLevel logger_level_from_string(const char* level_str) {
-    LogLevel level = ERROR;
-    if (strcmp(level_str, "DEBUG") == 0) {
+    LogLevel level = FATAL;
+    if (strcmp(level_str, "TRACE") == 0) {
+        level = TRACE;
+    } else if (strcmp(level_str, "DEBUG") == 0) {
         level = DEBUG;
     } else if (strcmp(level_str, "INFO") == 0) {
         level = INFO;
@@ -41,8 +49,10 @@ LogLevel logger_level_from_string(const char* level_str) {
         level = WARNING;
     } else if (strcmp(level_str, "ERROR") == 0) {
         level = ERROR;
+    } else if (strcmp(level_str, "FATAL") == 0) {
+        level = FATAL;
     } else {
-        logger(FATAL, __func__, "Unknown logger level: %s", level);
+        logger(FATAL, __func__, "Unknown logger level: %s", level_str);
         exit(1);
     }
     return level;
@@ -64,7 +74,7 @@ void logger(LogLevel level, const char* function_name, const char *message, ...)
     strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", localtime(&now));
     const char *level_str = logger_level_to_string(level);
 
-    fprintf(stderr, "%s | %s | %s: ", time_str, level_str, function_name);
+    fprintf(stderr, "%s | %s | %s | ", time_str, level_str, function_name);
 
     va_list args;
     va_start(args, message);
--- src/main.c
+++ src/main.c
@@ -1,12 +1,59 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "logger.h"
 
-int main() {
-    logger_set_level(DEBUG);
+void print_usage(const char *program_name) {
+    fprintf(stderr, "Usage: %s [options]\n", program_name);
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "  -h, --help              Display this help message and exit\n");
+    fprintf(stderr, "  -l, --log <log_level>   Set the log level (TRACE, DEBUG, INFO, WARNING, ERROR, FATAL)\n");
+}
 
+int parse_args(int argc, char* argv[]) {
+    int err_code = 1;
+    int opt;
+    static struct option long_options[] = {
+        {"log", required_argument, 0, 'l'},
+        {"help", no_argument, 0, 'h'},
+        {0, 0, 0, 0}
+    };
+
+    while ((opt = getopt_long(argc, argv, "l:h", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'l':
+                logger_set_level(logger_level_from_string(optarg));
+                err_code = 0;
+                break;
+            case 'h':
+                print_usage(argv[0]);
+                exit(0);
+            default:
+                print_usage(argv[0]);
+                err_code = 1;
+                break;
+        }
+    }
+
+    return err_code;
+}
+
+int main(int argc, char* argv[]) {
+    int err_code = 0;
+
+    err_code = parse_args(argc, argv);
+    if (err_code != 0) {
+        return err_code;
+    }
+
+    logger(TRACE, __func__, "Trace");
     logger(DEBUG, __func__, "Debug");
     logger(INFO, __func__, "Info");
     logger(WARNING, __func__, "Warning");
     logger(ERROR, __func__, "Error");
+    logger(FATAL, __func__, "Fatal");
 
     logger(INFO, __func__, "Placeholder test: %d + %d = %d", 2, 3, 2 + 3);
     logger(INFO, __func__, "Placeholder test: %c", 'c');
